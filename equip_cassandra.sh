db01a11a6a6b3f5e260217798545ea9b53ff5d39
jbellmann <joerg.bellmann@zalando.de>
2014-11-23 22:05:42 +0100

    add cassandra

---
+++ equip_cassandra.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+#
+#
+#
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo wget -q -O - http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+
+sudo sh -c 'sudo echo "deb http://debian.datastax.com/community/ stable main" > /etc/apt/sources.list.d/datastax.list'
+
+sudo apt-get update -y
+sudo apt-get install dsc12 -y
