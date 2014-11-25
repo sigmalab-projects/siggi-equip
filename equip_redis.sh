efbef23ef16af0a90a2847d91e83ada511c41b33
jbellmann <joerg.bellmann@zalando.de>
2014-11-25 20:55:15 +0100

    fix service name

--- equip_redis.sh
+++ equip_redis.sh
@@ -6,10 +6,10 @@
 #
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
-sudo apt-add-repository ppa:chris-lea/redis-server
+sudo apt-add-repository ppa:chris-lea/redis-server -y
 
 sudo apt-get update -y
 sudo apt-get install redis-server -y
 
 # stop service started by installation process
-sudo service redis stop
+sudo service redis-server stop
